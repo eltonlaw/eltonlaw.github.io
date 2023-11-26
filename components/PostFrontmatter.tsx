d70027fea4b8a3d4f057f9163d656ecb64804dcc
Elton Law <eltonlaw296@gmail.com>
2023-11-26 01:36:26 -0500

    refactor into nextjs app

---
+++ components/PostFrontmatter.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/PostFrontmatter.module.css'
+
+function PostFrontmatter( props : any) {
+  return (
+    <div className="postFrontmatter">
+      <h2 className={styles.postTitle}>{props.postTitle}</h2>
+      <span className={styles.postDate}>{props.postDate}</span>
+    </div>
+  )
+}
+
+export default PostFrontmatter
