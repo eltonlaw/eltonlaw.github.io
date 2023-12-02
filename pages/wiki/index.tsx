2255f686f30a08107cb8853797cb8fb6eb3171fd
Elton Law <eltonlaw296@gmail.com>
2023-12-01 23:46:43 -0500

    chore: share root container in AppContainer component

---
+++ pages/wiki/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import AppContainer from '../../components/AppContainer.tsx';
+
+const WikiIndex = () => {
+  const wikiPages = [
+  ];
+
+  return (
+    <AppContainer title="Elton Law" description="Wiki">
+      <h1>Wiki Index</h1>
+      <ul>
+        {wikiPages.map(page => (
+          <li key={page.path}>
+            <Link href={page.path}>{page.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </AppContainer>
+  );
+};
+
+export default WikiIndex;
