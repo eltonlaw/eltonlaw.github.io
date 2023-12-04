22638912d8f0747aa3bde7795eddeab2bfed539b
Elton Law <eltonlaw296@gmail.com>
2023-12-04 00:02:43 -0500

    Updates

--- _next/static/t_LHfOaXA6iHr3l4eBwxe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/8c61bbf7220f5504.css","static/chunks/pages/index-f34930dc52fc8689.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
