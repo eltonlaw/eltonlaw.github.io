31d05dd172db8416a5c314e23a845f52ce906f3b
Elton Law <eltonlaw296@gmail.com>
2023-12-20 22:23:43 -0500

    docs: add link to llm

--- app/wiki/page.tsx
+++ app/wiki/page.tsx
@@ -3,6 +3,7 @@ import PageIndex from '@/components/PageIndex';
 
 const pages = [
   { title: 'Clojure', path: '/wiki/clojure' },
+  { title: 'Large Language Models', path: '/wiki/large_language_models' },
 ];
 
 const Page = () => {
